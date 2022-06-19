48ac23973f2b9b50ff10668b123448d267e56279
Mootbing <50122069+Mootbing@users.noreply.github.com>
2022-06-19 18:16:06 -0400

    custom about page acutally working!!!!

--- src/App.js
+++ src/App.js
@@ -1,46 +1,50 @@
-import React, {Suspense, useRef, useState} from "react";
+import React, {Suspense, useRef, useState, useEffect} from "react";
 import "./App.css"
 
-import {Canvas, useFrame} from "react-three-fiber";
-
-import {useSpring, a} from "@react-spring/three";
+import {Canvas, useFrame, useThree} from "react-three-fiber";
 
 import { softShadows } from "drei/softShadows";
-import {MeshWobbleMaterial} from "drei/MeshWobbleMaterial";
 import { OrbitControls } from "drei/OrbitControls";
 
 import Model from './components/Model';
-
-const SpinningBox = ({position, args, speed, color}) => {
-
-  const mesh = useRef(null);
-
-  const [expand, setExpand] = useState(false);
-
-  const props = useSpring({
-    scale: expand ? [1.4, 1.4, 1.4] : [1, 1, 1],
-  });
-
-  useFrame(
-    () => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01)
-  );
-
-  return (
-    // <primitive object={fbx} />
-    <a.mesh castShadow onClick={() => {setExpand(!expand)}} ref={mesh} position={position} scale={props.scale}>
-      <boxBufferGeometry attach="geometry" args={args}/>
-      <MeshWobbleMaterial attach="material" speed={speed} factor={0.6} color={color}/>
-    </a.mesh>
-  );
-}
 
 softShadows();
 
 function App() {
 
+  let currentY = 0;
+
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  const handleScroll = () => {
+    setScrollPosition(window.pageYOffset);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const [lastScrollPosition, setLastScrollPosition] = useState(0);
+
+  const perFrame = (ref, callback) => {
+    const pendingPos = ((scrollPosition * 0.01)) % 6;
+    ref.current.scale.z = ref.current.scale.x = ref.current.scale.y = (pendingPos <= 3 ? pendingPos : 3 - (pendingPos - 3));
+
+    if (Math.abs(lastScrollPosition - pendingPos) > 1)
+    {
+      callback(lastScrollPosition, pendingPos);
+    }
+
+    setLastScrollPosition(pendingPos);
+  }
+
   return (
-    <div className="App" style={{position: "absolute", width: "100%", height:  "100%"}}>
-      <Canvas shadows colorManagement camera={{position: [-5, 2, 10], fov: 40}} className="fp">
+    <div className="App" style={{width: "100%", height: "100%", position: "absolute"}}>
+      <Canvas className="canvas-frame" shadows camera={{position: [-5, 2, 10], fov: 40}} style={{top: "0", left: "0", position: "fixed"}}>
         <ambientLight intensity={0.3}></ambientLight>
         <directionalLight
           castShadow
@@ -56,28 +60,52 @@ function App() {
           shadow-camera-top={-10}
           shadow-camera-bottom={10}
         />
-        <pointLight position={[-10, 0, -20]} intensity={1.5}/>
+        <pointLight position={[10, 0, 20]} intensity={1.5}/>
 
         <group>
           <mesh receiveShadow rotation={[-Math.PI/2, 0, 0]} position={[0, -1, 0]}>
             <planeBufferGeometry attach="geometry" args={[100, 100]} />
             <shadowMaterial attach="material" opacity="0.3"/>
-            {/* <meshStandardMaterial attach="material" color="white" /> */}
           </mesh>
         </group>
 
-        <SpinningBox position={[0, 1, 2]} args={[3, 2, 1]} speed={3} color="lightblue"/>
-        <SpinningBox position={[2, 4, 1]} args={[3, 2, 1]} speed={2}  color="pink"/>
-        <SpinningBox position={[4, 3, 5]} args={[3, 2, 1]} speed={6}  color="orange"/>
-
         <Suspense>
-          <Model position={[0, -1, 0]}/>
+          <Model callback={perFrame}/>
         </Suspense>
-        
-
-        <OrbitControls />
       </Canvas>
-      
+
+      <main>
+        <div class="text-over">
+          <p style={{fontSize: "100px"}}>
+            <br />
+            Jason
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            Larry
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            Agni
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            Sushant
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+          </p>
+        </div>
+      </main>
     </div>
   );
 }
